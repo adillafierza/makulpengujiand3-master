b745fbda41244793bf7500107b32901358c939d1
adillafierza <adillafierza@gmail.com>
2014-05-09 01:00:49 +0700

    UI Test

---
+++ C#/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCalculatorv1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        double a = 9;
+        double b = 3;
+
+        MainWindow mw = new MainWindow();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double actual = 12;
+            double expected = mw.Penjumlahan(a, b);
+            Assert.AreEqual(actual, expected);
+
+        }
+
+
+    }
+}
