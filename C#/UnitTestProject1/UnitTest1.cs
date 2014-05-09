0748f512f439f1e65d76cabe32c3247d6352d67b
adillafierza <adillafierza@gmail.com>
2014-05-09 07:14:05 +0700

    adding unitTest method

--- C#/UnitTestProject1/UnitTest1.cs
+++ C#/UnitTestProject1/UnitTest1.cs
@@ -13,10 +13,37 @@ namespace UnitTestProject1
         MainWindow mw = new MainWindow();
 
         [TestMethod]
-        public void TestMethod1()
+        public void TestPenjumlahan()
         {
             double actual = 12;
             double expected = mw.Penjumlahan(a, b);
+            Assert.AreEqual(actual, expected);
+
+        }
+
+        [TestMethod]
+        public void TestPengurangan()
+        {
+            double actual = 6;
+            double expected = mw.Pengurangan(a, b);
+            Assert.AreEqual(actual, expected);
+
+        }
+
+        [TestMethod]
+        public void TestPerkalian()
+        {
+            double actual = 27;
+            double expected = mw.Perkalian(a, b);
+            Assert.AreEqual(actual, expected);
+
+        }
+
+        [TestMethod]
+        public void TestPembagian()
+        {
+            double actual = 3;
+            double expected = mw.Pembagian(a, b);
             Assert.AreEqual(actual, expected);
 
         }
